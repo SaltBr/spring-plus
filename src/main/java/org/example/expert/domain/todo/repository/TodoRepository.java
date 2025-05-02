1b212ac1168af2bf1902453db3372e3e849431b5
SaltBr <snowgriff123@gmail.com>
2025-05-02 17:56:56 +0900

    Level1 : 3. 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+            "WHERE (:weather IS NULL OR t.weather = :weather) " +
+            "AND (:updatedStart IS NULL OR t.modifiedAt >= :updatedStart)" +
+            "AND (:updatedEnd IS NULL OR t.modifiedAt <= :updatedEnd)" +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByFilter(
+            @Param("weather") String weather,
+            @Param("updatedStart") LocalDateTime updatedStart,
+            @Param("updatedEnd") LocalDateTime updatedEnd,
+            Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
